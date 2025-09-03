28bfbd32e54e78d98f62926357ab55e7b2a6309e
Osvaldo <23030011@itcelaya.edu.mx>
2025-09-02 21:47:15 -0600

    Subiendo el apartado final del Proyecto TAP ATM

--- practica1Cajeroo/src/main/java/com/mycompany/practica1cajeroo/Practica1Cajeroo.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.practica1cajeroo;
-public class Practica1Cajeroo {
-
-    public static void main(String[] args) {
-        metodosCajero cajero = new metodosCajero();
-        cajero.ingresarUsuario();
-        cajero.leerMovimiento();
-    }
-}
